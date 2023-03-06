22334875c6de960411eb5ea0e247905c819c1e1f
Halis Manaz <halismanaz@hotmail.com>
2023-03-06 08:50:36 +0300

    MongoDB class is created

---
+++ db.py
@@ -0,0 +1,21 @@
+import os
+from typing import Optional
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load variables from .env file
+load_dotenv()
+
+
+class MongoDB:
+    def __init__(self):
+        username = os.getenv("MONGODB_USERNAME")
+        password = os.getenv("MONGODB_PASSWORD")
+        cluster = os.getenv("MONGODB_CLUSTER")
+        database_name = os.getenv("MONGODB_DATABASE")
+        collection_name = os.getenv("MONGODB_COLLECTION")
+        conn_str = f"mongodb+srv://{username}:{password}@{cluster}.mongodb.net/test"
+        self.client = MongoClient(conn_str)
+        self.db = self.client[database_name]
+        self.collection = self.db[collection_name]
